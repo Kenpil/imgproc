1746f48700274340c2345c9fb582166ceeab9857
Kenpil <42032640+Kenpil@users.noreply.github.com>
2020-06-25 01:18:01 +0900

    Add files via upload

--- practice/xmeansBrainIshioka.m
+++ practice/xmeansBrainIshioka.m
@@ -60,7 +60,7 @@ clc;
 
 pointLength = 1800; % 乱数生成する点の個数
 actPlaceM = zeros(pointLength,2);
-KTrue = 5; % 正解のクラスタの数
+KTrue = 8; % 正解のクラスタの数
 
 % pointLength個の点をKTrue個のクラスタになるよう乱数で求める
 for i = 1:KTrue
@@ -75,13 +75,15 @@ end
 
 % 乱数生成してみた点を表示
 figure;
+subplot(1,3,1);
 scatter(actPlaceM(:,1),actPlaceM(:,2), '.','red');
 title(['Original Points (',num2str(KTrue),' clusters)']);
 
 % 正しいクラスタ数(KTrue個)でクラスタ数を明示して，従来のk-meansクラスタリングをしてみる
 idx = kmeans(actPlaceM, KTrue);
 % KTrue個のクラスタに応じた点の分布を図時
-figure;
+%figure;
+subplot(1,3,2);
 gscatter(actPlaceM(:,1),actPlaceM(:,2),idx);
 title(['K-means Points (',num2str(KTrue),' clusters)']);
 
@@ -89,29 +91,36 @@ title(['K-means Points (',num2str(KTrue),' clusters)']);
 % save('C:\Users\Dell\WinRobots\develop\hw\biology\言語学C/actPlaceM.mat', 'actPlaceM');
 
 % x-meansにより，クラスタ数を与えず，自動でクラスタ数を判別し，さらに点を分ける
+% 第1引数はデータの（データ数,次元）行列
+% xmeans()の呼び出し時には第2引数を1で初期化
+% xkはクラスタリング結果のクラスタ数，xIndexはデータの属するクラスタ番号
 [xK,xIndex] = xmeans(actPlaceM,1);
 % 求めたクラスタ数
 xK
 % 求めたクラスタ数に応じた点の分布を図示
-figure;
+%figure;
+subplot(1,3,3);
 gscatter(actPlaceM(:,1),actPlaceM(:,2),xIndex);
 title(['X-means Points (Auto Determined ',num2str(xK),' clusters)']);
 
 %%
 
+% xは(データ数, 次元)のデータ行列例えば(x,y)2次元分布なら(点1のx座標1,点1のy座標1; 点2のx座標1,点2のy座標1; ...; 点nのx座標1,点nのy座標1)となる．
+% prepKは呼び出し時に1として初期化する．
+% 返り値 xK: x-meansクラスタリング結果のクラスタ数
+% 返り値 xIndex: x-meansクラスタリング結果の各データ点の属するクラスタ番号インデックス
 function [xK, xIndex] = xmeans(x,preK)
-    R = length(x(:,1));
-    M = length(x(1,:));
-    index = ones(R,1);
+    R = length(x(:,1)); % クラスタiの点数
+    M = length(x(1,:)); % 点の次元，2次元平面上に点が散らばるならM=2
+    index = ones(R,1); % 点がどのクラスタに属するか
     xK = 1;
     xIndex = ones(R,1);
     bicOri = bic(x,1,index);
     idxChild = kmeans(x, 2);
-%     figure;
-%     gscatter(x(:,1),x(:,2),idxChild);
  
     bicChild = bic(x,2,idxChild);
-%    bicOri
+    
+    % bic(クラスタiそのまま) > bic(クラスタiを2分割した時) なら2分割した方が良い->再帰的に分割を続行
     if(bicOri > bicChild)
         Rchild = zeros(2,1);
         for i = 1:R
@@ -130,11 +139,12 @@ function [xK, xIndex] = xmeans(x,preK)
                 tmp2 = tmp2 + 1;
             end
         end
-        txt = sprintf('split! preK: %f',preK);
+        
+        % 2分割後のクラスタそれぞれに対し，再帰的にx-meansを行い分割を続ける
         [xKChild1,xIndexChild1] = xmeans(xChild1,preK);
-        txt = sprintf('xKChild1: %f, preK: %f',xKChild1, preK);
         [xKChild2,xIndexChild2] = xmeans(xChild2,preK + xKChild1);
-        txt = sprintf('xKChild2: %f, preK: %f',xKChild2, preK);
+        
+        % 返り値を整理
         tmp1 = 1;
         tmp2 = 1;
         for i = 1:R
@@ -154,11 +164,12 @@ end
 
 %% 
 
+
 function BIC = bic(x,K,index)
     R = length(x(:,1)); % 要素の数:x(要素数,次元)
     M = length(x(1,:)); % 要素の次元
     Ri = zeros(K,1);
-    mui = zeros(K,M); % i番目クラスタの平均座標
+    mui = zeros(K,M); % i番目クラスタの平均座標，i番目のクラスタを2分割したら，(2,M)行列となり，[mu_x1 mu_y1; mu_x2 mu_y2]となる．
     BIC = 0;
     
     for i = 1:R
@@ -172,17 +183,19 @@ function BIC = bic(x,K,index)
     for i = 1:K
         mui(i,:) = mui(i,:) / Ri(i,1);
     end
-%    Ri
-%    mui
     
+    % クラスタi全体のBIC
+    % BIC = -2log[クラスタiの最尤推定量] + M(M+3)log(クラスタiの点の数)/2
     if(K == 1)
         x_mu = zeros(R,2);
+        % x - mu ベクトルx_mu
         for i = 1:M
             x_mu(:,i) = x(:,i) - mui(1,i);
         end
         Vi = x_mu'*x_mu/R;
         ViInv = inv(Vi);
         l1 = 0;
+        % i全体の点のsum((x-mu)^(t)V^(-1)(x-mu))
         for i = 1:R
             l1 = l1 + x_mu(i,:)*ViInv*x_mu(i,:)';
         end
@@ -190,11 +203,15 @@ function BIC = bic(x,K,index)
         BIC = -2*(-R*M*log(2*pi)/2 - R*log(det(Vi))/2 + l1) + M*(M+3)*log(R)/2;
     end
     
+    
+    % クラスタiを2分割した時のクラスタiのBIC
+    % BIC = -2log[点ごとに2分割後に対応する平均・分散を用いたクラスタiの最尤推定量] + M(M+3)log(クラスタiの点の数)
     if(K == 2)
         x1_mu = zeros(Ri(1),2);
         x2_mu = zeros(Ri(2),2);
         tmp1 = 1;
         tmp2 = 1;
+        % クラスタiの点がi_1,i_2のどちらに属すか判断し，x_mu = x - mu より平均から各点へのベクトルとして格納
         for i = 1:R
             if(index(i) == 1)
                 x1_mu(tmp1,1) = x(i,1) - mui(1,1);
@@ -206,6 +223,7 @@ function BIC = bic(x,K,index)
                 tmp2 = tmp2 + 1;
             end
         end
+        % i_1, i_2 それぞれの分散・共分散行列を求める
         Vi1 = x1_mu'*x1_mu/Ri(1);
         Vi1Inv = inv(Vi1);
         Vi2 = x2_mu'*x2_mu/Ri(2);
@@ -214,16 +232,18 @@ function BIC = bic(x,K,index)
         normMu1_mu2_2 = power(mui(1,1)-mui(2,1),2) + power(mui(1,2)-mui(2,2),2);
         detVi1 = det(Vi1);
         detVi2 = det(Vi2);
-        if detVi1 == 0 && detVi2 == 0
+        if detVi1 == 0 && detVi2 == 0 % 0割り対策
             beta = 0;
         else
             beta = power(normMu1_mu2_2/(detVi1+detVi2), 0.5);
         end
         alpha = 0.5/normcdf(beta);
+        
         l1 = 0;
         l2 = 0;
         tmp1 = 1;
         tmp2 = 1;
+        % i_1,i_2ごとのsum((x-mu)^(t)V^(-1)(x-mu))
         for i = 1:R
             if(index(i) == 1)
                 l1 = l1 + x1_mu(tmp1,:)*Vi1Inv*x1_mu(tmp1,:)';
@@ -235,9 +255,10 @@ function BIC = bic(x,K,index)
         end
         l1 = -l1/2;
         l2 = -l2/2;
+        % log[i_1,i_2ごとの最尤推定量]
         L1 = Ri(1)*log(alpha) - Ri(1)*M*log(2*pi)/2 - Ri(1)*log(det(Vi1))/2 + l1;
         L2 = Ri(2)*log(alpha) - Ri(2)*M*log(2*pi)/2 - Ri(2)*log(det(Vi2))/2 + l2;
-        % BIC = -2*(L1+L2) + M*(M+3)*log(Ri(1)) + M*(M+3)*log(Ri(2));
+        % i_1,i_2を統一
         BIC = -2*(L1+L2) + M*(M+3)*log(R);
     end
 end
